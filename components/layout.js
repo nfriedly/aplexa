f2407adf762f810e006b661dd9918458dd8ac4db
Nathan Friedly <nathan.friedly@tanium.com>
2018-10-02 23:15:49 -0400

    remove unused scss compiler

--- components/layout.js
+++ components/layout.js
@@ -1,6 +1,3 @@
-// eslint-disable-next-line no-unused-vars
-// import bootstrap from 'bootstrap/scss/bootstrap.scss';
-
 const Layout = ({ children }) => (
   <div className="container">
     {children}
--- next.config.js
+++ next.config.js
@@ -1,6 +1,4 @@
-const withSass = require('@zeit/next-sass');
-
-module.exports = withSass({
+module.exports = {
   webpack: (_config, { isServer }) => {
     const config = _config;
     if (!isServer) {
@@ -9,4 +7,4 @@ module.exports = withSass({
     }
     return config;
   },
-});
+};
